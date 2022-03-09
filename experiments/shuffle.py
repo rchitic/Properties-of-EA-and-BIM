75267c4f0439335118ffd550241104162d44b548
rchitic <rchitic17@gmail.com>
2022-03-09 15:26:52 +0100

    Update shuffle.py

--- experiments/shuffle.py
+++ experiments/shuffle.py
@@ -18,9 +18,10 @@ import cv2
 
 # torch
 import torch
-
-# own
-import params
+#os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+#print(torch.rand(1, device="cuda"))
+#torch.cuda.empty_cache()
+from torchvision import transforms
 from utils import create_torchmodel, prediction_preprocess, softmax
 
 # gpu
@@ -52,36 +53,15 @@ def shuffle(im, comb1, comb2, half):
 
 # params
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-SIN=False
-if SIN:
-	resnet50_SIN = create_torchmodel('ResNet50_SIN')
-	m = [resnet50_SIN]
-	networks = ['ResNet50_SIN']
-else:
-	networks = params.networks
-
-	bagnet9 = create_torchmodel('BagNet9')
-	bagnet17 = create_torchmodel('BagNet17')
-	bagnet33 = create_torchmodel('BagNet33')
-	vgg16 = create_torchmodel('VGG16')
-	vgg19 = create_torchmodel('VGG19')
-	resnet50 = create_torchmodel('ResNet50')
-	resnet101 = create_torchmodel('ResNet101')
-	resnet152 = create_torchmodel('ResNet152')
-	densenet121 = create_torchmodel('DenseNet121')
-	densenet169 = create_torchmodel('DenseNet169')
-	densenet201 = create_torchmodel('DenseNet201')
-	mobilenet = create_torchmodel('MobileNet')
-	mnasnet = create_torchmodel('MNASNet')
-	m = [vgg16,vgg19,resnet50,resnet101,resnet152,densenet121,densenet169,densenet201,mobilenet,mnasnet]
-
+networks = params.networks
 class_dict = params.class_dict
 names = params.names
+attack_name = 'EA'
 data_path = params.data_path
 results_path = params.results_path
-shuffle_combs_path = params.shuffle_combs_path
-
-attack_name = 'EA'
+m=[]
+for network in networks:
+	m.append(create_torchmodel(network))
 
 # Main
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -89,37 +69,40 @@ for i,model in enumerate(m):
 	network = networks[i]
 	for shuffle_size in [8,16,32,56,112]:
 		half = int(shuffle_size/2)
-		comb1 = np.load(shuffle_combs_path+str(shuffle_size)+'\\comb1.npy')
-		comb2 = np.load(shuffle_combs_path+str(shuffle_size)+'\\comb2.npy')
+		comb1 = np.load(data_path+'/shuffle_combs/'+str(shuffle_size)+'/comb1.npy')
+		comb2 = np.load(data_path+'/shuffle_combs/'+str(shuffle_size)+'/comb2.npy')
 
 		for name in names:
-			print(network,shuffle_size,name)
-			# Get ancestor and adversarial images
-			ancestor = cv2.imread(data_path+'/imagenet_{}/{}/{}.jpg'.format(name,name,name)) #BGR image
-			ancestor = cv2.resize(ancestor,(224,224))[:,:,::-1] #RGB image
-			ancestor = ancestor.astype(np.uint8)
-			ancestor = prediction_preprocess(Image.fromarray(ancestor)).cpu().detach().numpy()
-			results_loc = results_path+"\\{}".format(attack_name)
-			adv_network = np.load(results_loc + "\\{}\\attack\\{}\\image.npy".format(network,name))
+			for order in range(1,11):
+				results_loc = base_path + "results/{}".format(attack_name)
+				path = results_loc + "/{}/attack/{}/image{}.npy".format(network,name,str(order))
+				if os.path.exists(path):
+					print(network,shuffle_size,name)
+					# Get ancestor and adversarial images
+					ancestor = cv2.imread(data_path.format(name,name,str(order))) #BGR image
+					ancestor = cv2.resize(ancestor,(224,224))[:,:,::-1] #RGB image
+					ancestor = ancestor.astype(np.uint8)
+					ancestor = prediction_preprocess(Image.fromarray(ancestor)).cpu().detach().numpy()
+					adv_network = np.load(path)
 			
-			# Shuffle images
-			ancestor = shuffle(ancestor,comb1,comb2,half)
-			adv_network = shuffle(adv_network,comb1,comb2,half)
+					# Shuffle images
+					ancestor = shuffle(ancestor,comb1,comb2,half)
+					adv_network = shuffle(adv_network,comb1,comb2,half)
 
-			image_save_loc = results_loc +"\\{}\\shuffle_network\\{}\\{}\\images".format(network,shuffle_size,name)
-			np.save(image_save_loc + "\\ancestor.npy",ancestor)
-			np.save(image_save_loc + "\\adv_network.npy",adv_network)
+					image_save_loc = results_loc +"/{}/shuffle_network/{}/{}/images".format(network,shuffle_size,name)
+					np.save(image_save_loc + "/ancestor{}.npy".format(str(order)),ancestor)
+					np.save(image_save_loc + "/adv_network{}.npy".format(str(order)),adv_network)
 
-			# predict using pre-trained network
-			p_ancestor = run_network(model,ancestor)
-			p_adv_network = run_network(model,adv_network)
-			p_adv_bagnet9 = run_network(bagnet9,adv_network)
-			p_adv_bagnet17 = run_network(bagnet17,adv_network)
-			p_adv_bagnet33 = run_network(bagnet33,adv_network)
+					# predict using pre-trained network
+					p_ancestor = run_network(model,ancestor)
+					p_adv_network = run_network(model,adv_network)
+					p_adv_bagnet9 = run_network(bagnet9,adv_network)
+					p_adv_bagnet17 = run_network(bagnet17,adv_network)
+					p_adv_bagnet33 = run_network(bagnet33,adv_network)
 
-			pred_save_loc = results_loc +"\\{}\\shuffle_network\\{}\\{}\\preds".format(network,shuffle_size,name)
-			np.save(pred_save_loc + "\\ancestor.npy",p_ancestor)
-			np.save(pred_save_loc + "\\adv_network.npy",p_adv_network)
-			np.save(pred_save_loc + "\\adv_bagnet9.npy",p_adv_bagnet9)
-			np.save(pred_save_loc + "\\adv_bagnet17.npy",p_adv_bagnet17)
-			np.save(pred_save_loc + "\\adv_bagnet33.npy",p_adv_bagnet33)
+					pred_save_loc = results_loc +"/{}/shuffle_network/{}/{}/preds".format(network,shuffle_size,name)
+					np.save(pred_save_loc + "/ancestor{}.npy".format(str(order)),p_ancestor)
+					np.save(pred_save_loc + "/adv_network{}.npy".format(str(order)),p_adv_network)
+					np.save(pred_save_loc + "/adv_bagnet9{}.npy".format(str(order)),p_adv_bagnet9)
+					np.save(pred_save_loc + "/adv_bagnet17{}.npy".format(str(order)),p_adv_bagnet17)
+					np.save(pred_save_loc + "/adv_bagnet33{}.npy".format(str(order)),p_adv_bagnet33)
