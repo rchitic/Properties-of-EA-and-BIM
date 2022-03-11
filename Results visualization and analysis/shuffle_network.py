b565d45715879b22a0f8468d75d556bdd9299011
rchitic <rchitic17@gmail.com>
2022-03-11 22:29:46 +0100

    Update shuffle_network.py

--- Results visualization and analysis/shuffle_network.py
+++ Results visualization and analysis/shuffle_network.py
@@ -14,17 +14,19 @@ results_path = params.results_path
 def show_preds(attack_type,patch_size):
     for network in networks:
         for name in names:
-            print(f"{network} {name}:\n")
-            adv = np.load(results_path+'\\{}\\{}\\shuffle_network\\{}\\{}\\preds\\adv_network.npy'.format(attack_type,network,str(patch_size),name))
-            or_, target = class_dict[name][0], class_dict[name][1]
-            print(f" Ancestor & target classes, max pred: {adv[0,or_]},{adv[0,target]}, {decode_predictions(adv.reshape(1,1000))[0][0][1]}")
+            for order in range(1,11):
+                print(f"{network} {name} {order}:\n")
+                adv = np.load(results_path+'\\{}\\{}\\shuffle_network\\{}\\{}\\preds\\adv_network{}.npy'.format(attack_type,network,str(patch_size),name,order))
+                or_, target = class_dict[name][0], class_dict[name][1]
+                print(f" Ancestor & target classes, max pred: {adv[0,or_]},{adv[0,target]}, {decode_predictions(adv.reshape(1,1000))[0][0][1]}")
 
 def show_images(patch_size):
     for network in networks:
         print(f"{network}:\n")
         for name in names:
-            print(f"{network} {name}:\n")
-            adv = np.load(results_path+'\\{}\\{}\\shuffle_network\\{}\\{}\\images\\adv_network.npy'.format(attack_type,network,str(patch_size),name))
-            or_, target = class_dict[name][0], class_dict[name][1]
-            plt.imshow(_01(adv))
-            plt.show()
+            for order in range(1,11):
+                print(f"{network} {name} {order}:\n")
+                adv = np.load(results_path+'\\{}\\{}\\shuffle_network\\{}\\{}\\images\\adv_network{}.npy'.format(attack_type,network,str(patch_size),name,order))
+                or_, target = class_dict[name][0], class_dict[name][1]
+                plt.imshow(_01(adv))
+                plt.show()
